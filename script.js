a5c6b96591b4e4317c849289714f94e4cb2f6006
jefbiel <143344048+jefbiel@users.noreply.github.com>
2023-09-29 11:24:24 -0300

    Checkpoint

---
+++ script.js
@@ -0,0 +1,25 @@
+let nome = "Jeferson Gabriel"
+let idade = "18"
+let solteiro = true
+let valorProduto = "10.50"
+let linguagens = ['Java','C#','Python']
+
+let num1 = 2;
+let num2 = 10;
+
+console.log(soma)
+console.log(subtracao)
+console.log(mutiplicacao)
+console.log(divisao)
+
+let soma = num1 + num2;
+console.log("O resultado da soma é" + soma);
+
+let subtracao = num1 - num2;
+console.log("O resultado da subtração é" + subtracao);
+
+let mutiplicacao = num1 * num2;
+console.log("O resultado da mutipicação é" + mutiplicacao);
+
+let divisao = num1 / num2;
+console.log("O resultado da divisão é" + divisao);
